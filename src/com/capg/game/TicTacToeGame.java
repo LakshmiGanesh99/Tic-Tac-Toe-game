fd64e0beb7502dfbe906fc420a94babbdd509320
bhargavmln <bhargavmln@gmail.com>
2020-10-08 11:26:02 +0530

    [bharagvmln]add:added player choice method

--- src/com/capg/game/TicTacToeGame.java
+++ src/com/capg/game/TicTacToeGame.java
@@ -3,17 +3,29 @@ package com.capg.game;
 import java.util.Scanner;
 
 public class TicTacToeGame {
+	
 	static Scanner sc = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
+
 		createBoard();
+		System.out.println("Enter your choice 'X' or 'O'");
+		playerChoice(sc.next().charAt(0));
 	}
-	
+
 	public static char[] createBoard() {
 		char[] ticTacBoard = new char[10];
 		for (int i = 0; i < 10; i++) {
 			ticTacBoard[i] = ' ';
 		}
 		return ticTacBoard;
+	}
+
+	public static void playerChoice(char choice) {
+		if(choice == 'X') {
+			System.out.println("Computer choice is 'O'");
+		}else {
+			System.out.println("Computer choice is 'X'");
+		}
 	}
 }
