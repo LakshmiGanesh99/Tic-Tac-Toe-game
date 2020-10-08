c45f7a113e83d53a64b3f7486f06d5478fd7e49d
bhargavmln <bhargavmln@gmail.com>
2020-10-08 10:21:15 +0530

    [bhargavmln]add: added the create board method

---
+++ src/com/capg/game/TicTacToeGame.java
@@ -0,0 +1,19 @@
+package com.capg.game;
+
+import java.util.Scanner;
+
+public class TicTacToeGame {
+	static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		createBoard();
+	}
+	
+	public static char[] createBoard() {
+		char[] ticTacBoard = new char[10];
+		for (int i = 0; i < 10; i++) {
+			ticTacBoard[i] = ' ';
+		}
+		return ticTacBoard;
+	}
+}
